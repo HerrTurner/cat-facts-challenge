85f7d4e68d1b3c181f7fb52d703e53ad73c9393b
George Esc <georgeeturnere@gmail.com>
2024-08-09 22:28:24 -0600

    Commented dev tools

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+//import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
 
 const queryClient  = new QueryClient()
